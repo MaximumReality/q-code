7e6432b3e1023fb3e6b3df0b4f6113aa109edeb1
MaximumReality <lorith79@gmail.com>
2026-02-16 04:32:39 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'qcode-pyramid-v1.4'; // Bump this when you change assets!
+const CACHE_NAME = 'qcode-pyramid-v1.5'; // Bump this when you change assets!
 const ASSETS = [
   './',
   './index.html',
